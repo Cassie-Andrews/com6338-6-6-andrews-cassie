de43b7aff80c09a8572af59506037384d5aeab8f
Cassie Andrews <candrews1@ufl.edu>
2024-10-02 18:58:38 -0400

    troubleshooting esc key behavior

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 // implement a hamburger menu within the provided website. 
 // Your hamburger menu should only render on smaller screen sizes (those smaller than 800 pixels).
 
-
 var hamburgerBtn = document.querySelector("button.hamburger-btn");
 var hamburgerMenu = document.querySelector(".hamburger-menu");
-
 
 // To hide or show the hamburger menu, you will need to toggle the .show-menu class on the .hamburger-menu element.
 // when the menu is open, aria-expanded should be set to true on button.hamburger-btn, and set to false when the menu is closed.
@@ -18,10 +16,9 @@ function toggleMenu() {
     // when the menu is closed, aria-expanded should be set to false on button.hamburger-btn
 }
 
-// The hamburger menu should display the following behavior:
-// 1. When the button.hamburger-btn element is clicked on smaller screens, the hamburger menu should open/close. Again, toggling the show-menu class on the .hamburger-menu element should show/hide the menu.
 // ADD EVENT LISTENER FOR HAMBURGER BUTTON CLICK
 hamburgerBtn.addEventListener ("click", toggleMenu);
+// 1. When the hamburgerBtn is clicked on smaller screens, the hamburger menu should open/close.
 
 // ADD EVENT LISTENER FOR CLICK OUTSIDE MENU TO CLOSE 
 document.addEventListener ("click", function(event) {
@@ -33,14 +30,11 @@ document.addEventListener ("click", function(event) {
 });
 
 
-
-
-
 // ADD EVENT LISTENER FOR ESC KEY PRESS TO CLOSE MENU
-document.addEventListener ("keypress", function(e) {
+document.addEventListener ("keydown", function(event) {
     // 4. Pressing the Escape key when the menu is open should close the menu and focus button.hamburger-btn.
-    // open menu = var hamburgerMenu class list cont .show-menu
-    if (e.key === "Escape" && hamburgerMenu.classList.contains("show-menu")) {
+    // menu is open if the var hamburgerMenu class list cont .show-menu
+    if (event.key === "Escape" && hamburgerMenu.classList.contains("show-menu")) {
         toggleMenu(); // close menu
         hamburgerBtn.focus(); // focus button.hamburger-btn
     }
